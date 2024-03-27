f866323c48ec5b4a21bc2162145ded574ebb897d
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-27 23:26:18 +0100

    phone

---
+++ function.js
@@ -0,0 +1,13 @@
+// let none = document.getElementById("main");
+
+main.style.backgroundImage = "url('iphone.jpg')";
+
+function onn() {
+    // let none = document.getElementById("main");
+    main.style.backgroundImage = "url('iphone.jpg')";
+    if (main.style.backgroundImage.display === "block") {
+        main.style.backgroundImage.display = "block";
+    } else {
+        main.style.backgroundImage.display = "none";
+    }
+}
