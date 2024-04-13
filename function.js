5f8a6bfbb65c8874d9d0b2fee29dd494a14523f4
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-04-14 00:48:23 +0100

    password

--- function.js
+++ function.js
@@ -1,10 +1,49 @@
+let Ddate = document.getElementById("Ddate")
+let Dtime = document.getElementById("Dtime")
+let lockScreen = document.getElementById("lockScreen")
+let touch = document.getElementById("touch")
+let camera = document.getElementById("camera")
 document.getElementById("main").style.backgroundImage = "url('black.jpg')";
 let show = true
 date.style.display = "none"
 touch.style.display = "none"
-touch1.style.display = "none"
+camera.style.display = "none"
 Open.style.display = "none"
+password.style.display = "none"
+lockScreen.style.display = "block"
 
+touch.innerHTML = `<button><span class="material-symbols-outlined">flashlight_on</span></button>`
+camera.innerHTML = `<button><i class="fa-solid fa-camera"></i></button>`
+
+console.log(touch.innerHTML);
+console.log(camera.innerHTML);
+
+function dDate() {
+    setInterval(() => {
+        let date = new Date
+
+        // Ddate.innerHTML =`${date.getMonth()}, ${date.getDay()}`
+        Ddate.innerHTML =`${date.toDateString()}`
+    }, 1000);
+}
+
+dDate()
+
+function dTime() {
+    setInterval(() => {
+        let date = new Date();
+        Dtime.innerHTML = `${date.getHours()} : ${date.getMinutes()}`
+
+        h = date.getHours()
+        m = date.getMinutes()
+
+        h = h < 10 ? "0" + h : h
+        m = m < 10 ? "0" + m : m
+        Dtime.innerHTML = `${h} : ${m} `
+    }, 1000);
+}
+
+dTime()
 
 function onn() {
     console.log(show);
@@ -13,7 +52,7 @@ function onn() {
         show = false
         date.style.display = "block"
         touch.style.display = "block"
-        touch1.style.display = "block"
+        camera.style.display = "block"
         Open.style.display = "block"
     }
     else {
@@ -21,7 +60,16 @@ function onn() {
         show = true
         date.style.display = "none"
         touch.style.display = "none"
-        touch1.style.display = "none"
+        camera.style.display = "none"
         Open.style.display = "none"
     }
+
+}
+
+function openphone() {
+    password.style.display == "none" ? password.style.display = "block" : password.style.display = "none";
+    lockScreen.style.display == "block" ? lockScreen.style.display = "none" : lockScreen.style.display = "block";
+
+    // touch.innerHTML === `<button><span class="material-symbols-outlined">flashlight_on</span></button>` ? touch.innerHTML = "Emergency" : touch.innerHTML = `<button><span class="material-symbols-outlined">flashlight_on</span></button>`;
+    // camera.innerHTML === `<button><i class="fa-solid fa-camera"></i></button>` ? camera.innerHTML = 'Cancel' : camera.innerHTML = `<button><i class="fa-solid fa-camera"></i></button>`
 }
