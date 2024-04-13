af028f5d42a5e4ffaf990bae2805893151bb2d49
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-04-13 22:23:06 +0100

    time

--- function.js
+++ function.js
@@ -1,14 +1,27 @@
 document.getElementById("main").style.backgroundImage = "url('black.jpg')";
 let show = true
+date.style.display = "none"
+touch.style.display = "none"
+touch1.style.display = "none"
+Open.style.display = "none"
+
 
 function onn() {
     console.log(show);
     if (show == true) {
         document.getElementById("main").style.backgroundImage = "url('iphone.jpg')"
-        show = false  
+        show = false
+        date.style.display = "block"
+        touch.style.display = "block"
+        touch1.style.display = "block"
+        Open.style.display = "block"
     }
     else {
-        document.getElementById("main").style.backgroundImage =  "url('black.jpg')";
-        show =  true
+        document.getElementById("main").style.backgroundImage = "url('black.jpg')";
+        show = true
+        date.style.display = "none"
+        touch.style.display = "none"
+        touch1.style.display = "none"
+        Open.style.display = "none"
     }
 }
